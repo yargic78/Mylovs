bbea1257ed761bfb8716337eb6b34e995b71f4c6
yargic78 <ahmetselimoksz02@gmail.com>
2025-07-05 04:46:04 +0300

    Update and rename script.css to script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const imageList = [
+  { image: 'images/img1.jpg', text: '1 Ay' },
+  { image: 'images/img2.jpg', text: '1 Seneye' },
+  { image: 'images/img3.jpg', text: '10 Seneye' },
+  { image: 'images/img4.jpg', text: 'Bir Ömüre Dönüşecek' }
+];
+
+let index = 0;
+const bg = document.getElementById('bg');
+const text = document.getElementById('text');
+const btn = document.getElementById('nextBtn');
+
+btn.addEventListener('click', () => {
+  index++;
+  if (index < imageList.length) {
+    bg.style.backgroundImage = `url('${imageList[index].image}')`;
+    text.innerText = imageList[index].text;
+  } else {
+    text.innerText = 'Sonsuza Kadar ❤️';
+    btn.style.display = 'none';
+  }
+});
+
+function startMusic() {
+  const music = document.getElementById('bgMusic');
+  if (music.paused) {
+    music.play().catch(e => console.log("Müzik başlamadı:", e));
+  }
+}
